9fef80fcb93b6ffd88eda1651447fe12df1c5e3f
Casper Bang-Hansen <casperbanghansen@gmail.com>
2023-02-16 17:00:31 +0100

    Checking errors in database

--- api/shared_src/__init__.py
+++ api/shared_src/__init__.py
@@ -1,22 +1,4 @@
-FAILED = "BASIC:"
-Exceptions = {}
-try:
-    from . import BlobCommunication as BlobCommunication
-except Exception as e:
-    Exceptions["BlobCommunication"] = repr(e)
-    FAILED += " BlobCommunication"
-try:
-    from . import HandleInput as HandleInput
-except Exception as e:
-    Exceptions["HandleInput"] = repr(e)
-    FAILED += " HandleInput"
-try:
-    from . import xarray_operations as xarray_operations
-except Exception as e:
-    Exceptions["xarray_operations"] = repr(e)
-    FAILED += " xarray_operations"
-try:
-    from . import databases as databases
-except Exception as e:
-    Exceptions["databases"] = repr(e)
-    FAILED += " databases"
+from . import BlobCommunication as BlobCommunication
+from . import HandleInput as HandleInput
+from . import xarray_operations as xarray_operations
+from . import databases as databases
